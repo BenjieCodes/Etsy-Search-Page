03d198a5332896a0b89c2ba3b2d52b53ba5038a1
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-16 09:24:44 -0400

    Organized images in the container - now to work on CSS.

--- src/js/main.js
+++ src/js/main.js
@@ -12,10 +12,10 @@ etsyItems.results.forEach(function (item){
   var etsyImage = item.Images[0];
   var imgUrl;
   if (etsyImage === undefined ) {
-    imgUrl = "http://placehold.it/75x75";
+    imgUrl = "http://placehold.it/170x135";
   }
   else {
-    imgUrl = etsyImage.url_75x75;
+    imgUrl = etsyImage.url_170x135;
   }
   // object's item name
   var itemName = item.title;
